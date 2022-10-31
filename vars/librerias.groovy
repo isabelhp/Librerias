e72bcc6f8956c7885ab25291909116c534a8187f
isabel.hp <i.horcajada.pastor@accenture.com>
2022-10-31 15:19:07 +0100

    aaaaaaaa

--- vars/librerias.groovy
+++ vars/librerias.groovy
@@ -1,3 +1,5 @@
-def call(String name, String dayOfWeek) {
-    sh "echo Hola ${name}. Esto es una prueba de ${dayOfWeek}."
-}
+//def call(String name, String dayOfWeek) {
+//    sh "echo Hola ${name}. Esto es una prueba de ${dayOfWeek}."
+//}
+
+def version = readYaml file: "release.yml"
